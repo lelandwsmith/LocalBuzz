7550330f9ca16c09bf7bfaed577a70d961326360
Amanda Le <amandale656@gmail.com>
2012-10-01 22:14:13 -0400

    Added Past/CurrentEventViewController and DetailEventDescriptionViewController. Now can go from the tab view to detailed description view.

---
+++ CurrentEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  CurrentEventViewController.h
+//  LocalBuzz
+//
+//  Created by Amanda Le on 10/1/12.
+//  Copyright (c) 2012 Vincent Leung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrentEventViewController : UITableViewController
+
+@end
---
+++ LocalBuzz/DetailEventDescriptionViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailEventDescriptionViewController.m
+//  LocalBuzz
+//
+//  Created by Amanda Le on 10/1/12.
+//  Copyright (c) 2012 Vincent Leung. All rights reserved.
+//
+
+#import "DetailEventDescriptionViewController.h"
+
+@interface DetailEventDescriptionViewController ()
+
+@end
+
+@implementation DetailEventDescriptionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
