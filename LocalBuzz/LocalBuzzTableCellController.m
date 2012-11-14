2f7c11dda68f2dc281e8798c7ef9ae3053719716
fuzichao <zichaof@gmail.com>
2012-11-13 23:48:09 -0500

    customized table cell

---
+++ LocalBuzz/LocalBuzzTableCellController.m
@@ -0,0 +1,29 @@
+//
+//  LocalBuzzTableCellController.m
+//  LocalBuzz
+//
+//  Created by Zichao Fu on 12-11-13.
+//  Copyright (c) 2012年 Vincent Leung. All rights reserved.
+//
+
+#import "LocalBuzzTableCellController.h"
+
+@implementation LocalBuzzTableCellController
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
